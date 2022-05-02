f2f43d6ae4a9b2731371d2fe95619988229f4250
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-05-02 15:30:14 +1000

    creating inquirer function and card layout

--- server.js
+++ server.js
@@ -1,2 +1,69 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const teamMember = [];
+
+const Questions = async () => {
+    const Member = await inquirer.prompt([
+        {
+            type: "input",
+            message: "What's your name?",
+            name: "name",
+          },
+          {
+            type: "input",
+            message: "What's your ID?",
+            name: "id",
+          },
+          {
+            type: "input",
+            message: "What's your email address?",
+            name: "email",
+          },
+          {
+            type: "list",
+            message: "What's your role?",
+            name: "role",
+            choices: ["Manager", "Employee", "Intern"],
+          },
+    ]);
+
+    if (Member.role == "Manager") {
+        const officeNumber = await inquirer.prompt([
+            {
+                type: "input",
+                message: "What's your office number",
+                name: "officeNumber",
+            },
+        ])
+        const manager = new Manager(
+            Member.name, Member.id, Member.email, officeNumber.officeNumber);
+            teamMember.push(manager);
+        } else if (Member.role == "Engineer") {
+            const GitHub = await inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What's your GitHub username?",
+                    name: "github",
+                }
+            ])
+            const engineer = new Engineer(
+                Member.name, Member.id, Member.email, GitHub.github);
+                teamMember.push(engineer);
+        } else if (Member.role == "Intern") {
+            const School = await inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What school did you attend",
+                    name: "school",
+                }
+            ])
+            const intern = new Intern(
+                Member.name, Member.id, Member.email, School.school);
+                teamMember.push(intern)
+        }
+};
